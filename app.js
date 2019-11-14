bd45d79282988b71dbbf4cef05984180b94d434a
cflo <cflo013@gmail.com>
2019-11-14 16:00:45 +0800

    Add Mongoose connection and create Diner model

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/diner', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+//  import Diner model
+const Diner = require('./models/diner')
 
 app.get('/', (req, res) => {
-  res.send('Diner List CRUD assignment')
+  res.send('Diner List CRUD')
 })
 
 app.listen(port, () => {

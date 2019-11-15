7b6907c98c09e2369ee7343332bdea5af3c999dc
cflo <cflo013@gmail.com>
2019-11-15 10:11:45 +0800

    Add all routes

--- app.js
+++ app.js
@@ -17,8 +17,38 @@ db.once('open', () => {
 //  import Diner model
 const Diner = require('./models/diner')
 
+//  Routes
+//  Home
 app.get('/', (req, res) => {
-  res.send('Diner List CRUD')
+  res.send('list all Diners, home')
+})
+//  list all
+app.get('/diners', (req, res) => {
+  res.send('list all Diners')
+})
+//  list one detail
+app.get('/diners/:id', (req, res) => {
+  res.send('list one diner detail')
+})
+//  enter add new one page
+app.get('/diners/new', (req, res) => {
+  res.send('add new one page')
+})
+//  do add new one action
+app.post('/diners', (req, res) => {
+  res.send('add new one action')
+})
+//  enter edit page
+app.get('/diners/:id/edit', (req, res) => {
+  res.send('edit one page')
+})
+//  do edit one action
+app.post('/diners/:id/edit', (req, res) => {
+  res.send('edit one action')
+})
+//  do delete one action
+app.post('/diners/:id/delete', (req, res) => {
+  res.send('delete one diner')
 })
 
 app.listen(port, () => {
